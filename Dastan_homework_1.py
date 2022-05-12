782db38cad4ac1f99bdd390d7a4abe2311f562c3
DastanSaliev <dastan.saliev.02@mail.ru>
2022-05-12 16:33:18 +0600

    Initial commit

---
+++ Dastan_homework_1.py
@@ -0,0 +1,33 @@
+class Cars:
+    brand = "Bmw, Mercedes"
+
+    def __init__(self, title, model, max_speed, color, weight, hp):
+        self.title = title
+        self.model = model
+        self.max_speed = max_speed
+        self.color = color
+        self.weight = weight
+        self.hp = hp
+
+    def start_engine(self):
+        print(f"{self.title} {self.model}  engine started!")
+
+    def stop_engine(self):
+        print(f"{self.title} {self.model} engine stop!")
+
+    def info(self):
+        print(f"{self.title} {self.model} {self.weight} {self.hp} {self.max_speed} {self.color} информация о машине!")
+
+
+mercedes = Cars("Mercedes", "S ClassG", 250, "black", "2.5 tons", 450 )
+mercedes.start_engine()
+bmw = Cars("Bmw", "M5 f90", 300, "blue", "2 tons", 600 )
+bmw.start_engine()
+
+mercedes = Cars("Mercedes", "S ClassG", 250, "black", "2.5 tons", 450 )
+bmw.stop_engine()
+mercedes.stop_engine()
+
+mercedes = Cars("Mercedes", "S ClassG", 250, "black", "2.5 tons", 450 )
+bmw.info()
+mercedes.info()
